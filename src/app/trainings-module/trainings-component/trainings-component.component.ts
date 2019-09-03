84d23ece966ccdf32455122812ac11bcce699112
Artem Shiliaev <shiliaev@statmath.de>
2019-09-03 15:33:23 +0200

    module

---
+++ src/app/trainings-module/trainings-component/trainings-component.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-trainings-component',
+  templateUrl: './trainings-component.component.html',
+  styleUrls: ['./trainings-component.component.css']
+})
+export class TrainingsComponentComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
---
+++ src/app/trainings-module/trainings-module.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { TrainingsComponentComponent } from './trainings-component/trainings-component.component';
+
+
+
+@NgModule({
+  declarations: [TrainingsComponentComponent],
+  imports: [
+    CommonModule
+  ]
+})
+export class TrainingsModuleModule { }
