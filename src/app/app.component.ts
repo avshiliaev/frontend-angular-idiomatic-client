4ca725356f4c4f9a46d01a97f27b049df0065fe0
Artem Shiliaev <shiliaev@statmath.de>
2019-09-04 11:45:12 +0200

    [class.disc]="t.discontinued"

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -12,17 +12,23 @@ export class AppComponent {
     {
       id:1,
       name: "Angular",
-      description: "description_1"
+      description: "description_1",
+      imgUrl: "assets/angular2-shield.svg",
+      discontinued: false
     },
     {
       id:2,
       name: "Docker",
-      description: "description_2"
+      description: "description_2",
+      imgUrl: "assets/docker.svg",
+      discontinued: true
     },
     {
       id:3,
       name: "Kubernetes",
-      description: "description_3"
+      description: "description_3",
+      imgUrl: "assets/Kubernetes.svg",
+      discontinued: false
     }
   ]; 
 
--- src/app/training.ts
+++ src/app/training.ts
@@ -1,5 +1,7 @@
 export interface Training {
     id:number,
     name:string,
-    description:string
+    description:string,
+    imgUrl: string,
+    discontinued: boolean
 }
