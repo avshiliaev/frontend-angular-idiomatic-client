3a21945901b2940fa74af1ceb76fe3e186ca4c01
Artem Shiliaev <shiliaev@statmath.de>
2019-09-04 12:01:12 +0200

    ngModel

--- src/app/trainings-module/trainings-module.module.ts
+++ src/app/trainings-module/trainings-module.module.ts
@@ -2,12 +2,14 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { TrainingsComponentComponent } from './trainings-component/trainings-component.component';
 import { TrainingsDetailComponent } from './trainings-detail/trainings-detail.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [TrainingsComponentComponent, TrainingsDetailComponent],
   exports: [TrainingsComponentComponent, TrainingsDetailComponent],
   imports: [ 
-    CommonModule 
+    CommonModule,
+    FormsModule 
   ]
 })
 export class TrainingsModuleModule { }
