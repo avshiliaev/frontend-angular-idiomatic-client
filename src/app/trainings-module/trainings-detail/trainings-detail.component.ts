cd44781132f42737eecdfb26c6567f63a72c5be5
Artem Shiliaev <shiliaev@statmath.de>
2019-09-04 10:16:27 +0200

    even binding 2

--- src/app/trainings-module/trainings-detail/trainings-detail.component.ts
+++ src/app/trainings-module/trainings-detail/trainings-detail.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, EventEmitter, Output, Input } from '@angular/core';
+import { Training } from "src/app/training"
 
 @Component({
   selector: 'app-trainings-detail',
@@ -6,6 +7,9 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./trainings-detail.component.css']
 })
 export class TrainingsDetailComponent implements OnInit {
+
+  @Input() selectedTraining: Training[];
+  //@Output() like = new EventEmitter<Training>();
 
   constructor() { }
 
