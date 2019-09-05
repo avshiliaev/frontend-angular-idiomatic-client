59c4521987c6feb2eee670bd7df254d389591958
Artem Shiliaev <shiliaev@statmath.de>
2019-09-05 10:08:35 +0200

    forms

--- src/app/trainings-module/trainings-detail/trainings-detail.component.ts
+++ src/app/trainings-module/trainings-detail/trainings-detail.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit, EventEmitter, Output, Input } from '@angular/core';
 import { Training } from "src/app/training";
+import { FormGroup, FormControl } from "@angular/forms"; 
 
 @Component({
   selector: 'app-trainings-detail',
@@ -10,11 +11,20 @@ export class TrainingsDetailComponent implements OnInit {
 
   @Input() selectedTraining: Training;
   //@Output() like = new EventEmitter<Training>();
+  public form: FormGroup; 
 
-  constructor() { }
+  constructor() { };
 
   ngOnInit() {
-  }
+    this.form = new FormGroup({
+      name: new FormControl(this.selectedTraining.name),
+      nextRun: new FormControl(this.selectedTraining.nextRun)
+    });
+  };
+ 
+  onSubmit() {
+    Object.assign(this.selectedTraining, this.form.value);
+  };
 
 
   
--- src/app/trainings-module/trainings-module.module.ts
+++ src/app/trainings-module/trainings-module.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { TrainingsComponentComponent } from './trainings-component/trainings-component.component';
 import { TrainingsDetailComponent } from './trainings-detail/trainings-detail.component';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { TakePlaceSoonPipe } from './take-place-soon.pipe';
 import { TrainingListRoutComponentComponent } from './training-list-rout-component/training-list-rout-component.component';
 import { TrainingDetailsRouteComponentComponent } from './training-details-route-component/training-details-route-component.component';
@@ -14,7 +14,8 @@ import { RouterModule } from '@angular/router';
   imports: [ 
     CommonModule, 
     FormsModule,
-    RouterModule 
+    RouterModule,
+    ReactiveFormsModule
   ]
 })
 export class TrainingsModuleModule { }
